4d62a27bfb7d048a46a355f1bbdaa3198edc3867
Muhammed Rashid N <85437745+rashidpkn@users.noreply.github.com>
2023-06-10 18:32:41 +0530

    cgcgftd

--- src/pages/common/Footer.jsx
+++ src/pages/common/Footer.jsx
@@ -1,80 +1,6 @@
-// import React from 'react'
-// import { useDispatch } from 'react-redux'
-// import { Link, useLocation } from 'react-router-dom'
-// import { setRoadMap } from '../../redux/slice/util'
+
 import { Discode, InstagramLogo, TiwtterLogo } from './NavBar'
-// import { Discount } from '@mui/icons-material'
-// function Footer() {
-//     return (
-//         <footer className='text-white bg-[#121212]'>
-//             <div className="flex flex-col lg:flex-row   border-y border-[#333] divide-x divide-[#333]">
-//                 <div className="px-[5%] w-full lg:w-1/2 h-[420px] flex flex-col justify-center items-center gap-3">
-//                     <div className="flex flex-col gap-3">
-//                         <h2 className='text-3xl md:text-5xl'>Stay Up To Date With<br/> Vapemonkeydubai.</h2>
-//                         <p className='text-justify'>Get the latest news and information about the Vapemonkeydubai NFT project.</p>
-//                     </div>
-//                     <div className="flex gap-3 justify-center flex-wrap">
-//                         <button className='w-[100px] h-[100px] rounded-full bg-[#ff0097] hover:h-[90px] hover:w-[90px] duration-200'>JOIN NOW</button>
-//                     </div>
-//                 </div>
-//                 <div className="w-full lg:w-1/2 h-[420px] flex flex-col divide-y divide-[#333]">
-//                     <Navigation />
-//                     <div className="w-full h-[120px] flex items-center justify-center lg:justify-start gap-10 px-0 lg:px-10 ">
-//                         <div className="flex flex-col">
-//                             <Link to={'/terms-and-privacy'}>
-//                                 <span>Terms & Privacy Policy</span>
-//                             </Link>
-//                             <span>Licensing</span>
-//                         </div>
-//                         <div className="flex gap-5 invert">
-//                             <a title='Social Media Links' aria-label='social Media links' target={'_blank'} rel="noreferrer" href='https://google.com' className="h-10 w-10 ">
-//                                 <InstagramLogo />
-//                             </a>
-//                             <a title='Social Media Links' aria-label='social Media links' target={'_blank'} rel="noreferrer" href='https://twitter.com/monkey_dubai' className="h-10 w-10 ">
-//                                 <TiwtterLogo />
-//                             </a>
-//                             <a title='Social Media Links' aria-label='social Media links' target={'_blank'} rel="noreferrer" href='https://discord.gg/NC3RXXh8' className="h-10 w-10 ">
-//                                 <Discode />
-//                             </a>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//             <CopyRight />
-//         </footer>
-//     )
-// }
 
-// export default Footer
-
-
-// function Navigation() {
-//     const dispatch = useDispatch()
-//     const {pathname} = useLocation()
-//     return (
-//         <div className="w-full h-[300px] flex flex-col p-[5%] text-sm ">
-//             <h2 className='tracking-[.2rem]'>NAVIGATION</h2>
-//             <ul className="flex flex-col flex-wrap w-1/2 h-3/4 text-4xl font-light gap-x-5">
-//                 <li className={`decoration-[#ff0097] ${pathname === '/' && 'line-through'} h-1/3 flex items-center`}><Link title='home' to={'/'}> Home </Link></li>
-//                 <li className={`decoration-[#ff0097] ${pathname === '/about' && 'line-through'} h-1/3 flex items-center`}><Link title='about' to={'/about'}> About </Link></li>
-//                 <li className={`decoration-[#ff0097] ${pathname === '/roadmaps' && 'line-through'} h-1/3 flex items-center cursor-pointer`} onClick={()=>{dispatch(setRoadMap(true))}}> Roadmaps </li>
-//                 {/* <li className={`decoration-[#ff0097] ${pathname === '/meta' && 'line-through'} h-1/3 flex items-center`}><Link title='metaverse' to={'/meta'}> Metaverse </Link></li> */}
-//                 <li className={`decoration-[#ff0097] ${pathname === '/contact' && 'line-through'} h-1/3 flex items-center`}><Link title='contact' to={'/contact'}> Contact </Link></li>
-//                 {/* <li className='h-1/3 flex items-center text-lg'><Link title='press and media kit' to={'/press'}> Press/Media Kit </Link></li> */}
-//             </ul>
-//         </div>
-//     )
-// }
-
-
-// function CopyRight() {
-//     return (
-//         <div className="flex justify-around items-center h-20">
-//             <span>® 2023. Vapemonkeydubai. All Rights Reserved</span>
-//             <span>Website by Vapemonkeydubai</span>
-//         </div>
-//     )
-// }
 
 
 import React from 'react'
@@ -127,9 +53,24 @@ function Footer() {
                 <p> © 2023. Vape Monkey Dubai. All Right Reserved </p>
                 <p>Website by Vape Monkey Dubai</p>
             </div>
+            <Whatsapp/>
         </footer>
 
     )
 }
 
 export default Footer
+
+const Whatsapp = () => {
+  return (
+
+    <a href="https://web.whatsapp.com/send?phone=971504327855&text=" target="_blank" rel="noopener noreferrer">
+      <div className="fixed bottom-16 right-3 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-green-500 text-white lg:bottom-10 lg:right-10">
+        <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium whatsapp-icon css-vubbuv" width='30px' focusable="false" fill='#fff' aria-hidden="true" viewBox="0 0 24 24" data-testid="WhatsAppIcon">
+          <path d="M16.75 13.96c.25.13.41.2.46.3.06.11.04.61-.21 1.18-.2.56-1.24 1.1-1.7 1.12-.46.02-.47.36-2.96-.73-2.49-1.09-3.99-3.75-4.11-3.92-.12-.17-.96-1.38-.92-2.61.05-1.22.69-1.8.95-2.04.24-.26.51-.29.68-.26h.47c.15 0 .36-.06.55.45l.69 1.87c.06.13.1.28.01.44l-.27.41-.39.42c-.12.12-.26.25-.12.5.12.26.62 1.09 1.32 1.78.91.88 1.71 1.17 1.95 1.3.24.14.39.12.54-.04l.81-.94c.19-.25.35-.19.58-.11l1.67.88M12 2a10 10 0 0 1 10 10 10 10 0 0 1-10 10c-1.97 0-3.8-.57-5.35-1.55L2 22l1.55-4.65A9.969 9.969 0 0 1 2 12 10 10 0 0 1 12 2m0 2a8 8 0 0 0-8 8c0 1.72.54 3.31 1.46 4.61L4.5 19.5l2.89-.96A7.95 7.95 0 0 0 12 20a8 8 0 0 0 8-8 8 8 0 0 0-8-8z"></path>
+        </svg>
+      </div>
+    </a>
+
+  )
+}
